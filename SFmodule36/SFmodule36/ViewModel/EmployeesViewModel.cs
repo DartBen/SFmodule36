25163b3c2b034df84f5c1a176d84a1c40b1bd3bb
DESKTOP-BC8P93V\Dima <ryabchikk@rambler.ru>
2023-05-27 18:19:01 +0300

    фильтрация

--- SFmodule36/SFmodule36/ViewModel/EmployeesViewModel.cs
+++ SFmodule36/SFmodule36/ViewModel/EmployeesViewModel.cs
@@ -2,28 +2,98 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SFmodule36.ViewModel
 {
-    class EmployeesViewModel
+    class EmployeesViewModel : INotifyPropertyChanged
     {
-        private EmployeeRepository _employeeRepository { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private EmployeeRepository _employeeRepository;
+        private ObservableCollection<Employee> _employees;
+        private string _filter;
+        private string _filterMessage;
+
         public EmployeesViewModel()
         {
             _employeeRepository = new EmployeeRepository();
+            FillListView();
+            FillFilterMessage();
         }
 
         public ObservableCollection<Employee> Employees
         {
             get
             {
-                return new ObservableCollection<Employee>
-                    (this._employeeRepository.GetAll());
+                return _employees;
+            }
+            set
+            {
+                _employees = value;
+                OnPropertyChanged();
             }
         }
 
+        public string Filter
+        {
+            get
+            {
+                return _filter;
+            }
+            set
+            {
+                _filter = value;
+                FillListView();
+                FillFilterMessage();
+            }
+        }
+
+        public string FilterMessage
+        {
+            get
+            {
+                return _filterMessage;
+            }
+            set
+            {
+                _filterMessage = value;
+                OnPropertyChanged();
+            }
+        }
+        private void FillListView()
+        {
+            if (!String.IsNullOrEmpty(_filter))
+            {
+                Employees = new ObservableCollection<Employee>(
+                  _employeeRepository
+                  .GetAll()
+                  .Where(v => v.FirstName.Contains(_filter)));
+            }
+            else
+                Employees = new ObservableCollection<Employee>(
+                  _employeeRepository.GetAll());
+        }
+
+        private void FillFilterMessage()
+        {
+            if (!String.IsNullOrEmpty(_filter))
+            {
+                FilterMessage = "По вашему запросу найдено: " + Employees.Count().ToString();
+            }
+            else
+            {
+                FilterMessage = "Введите данные для поиска";
+            }
+        }
     }
 }
